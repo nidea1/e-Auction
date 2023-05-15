b362ee04fd15153dc209737c11ed8656a42b24f8
nidea1 <crlidoruk@gmail.com>
2023-05-15 17:43:29 +0300

    old selling screen

---
+++ frontend/src/screens/UploadScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+
+function UploadScreen() {
+
+    return (
+        <>
+            <Outlet />
+        </>
+    )
+}
+
+export default UploadScreen
