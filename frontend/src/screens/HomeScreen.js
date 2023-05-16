7d9ef6da0ff4dee15cafcb9cb8f773c3495cb3d7
nidea1 <crlidoruk@gmail.com>
2023-05-16 20:47:48 +0300

    Added status choice

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Form } from 'react-bootstrap';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
@@ -14,11 +14,66 @@ function HomeScreen() {
     productReducers : { error, loading, products }
   } = useSelector((state) => state)
 
-  useEffect(() =>{
-    dispatch(listProducts())
-  }, [dispatch])
+  const statuses = [
+    {
+        'name': 'Ongoing',
+        'value': true,
+        'id': 1
+    },
+    {
+        'name': 'Finished',
+        'value': false,
+        'id': 2
+    },
+    {
+        'name': 'All Products',
+        'value': '',
+        'id': 3
+    }
+  ]
+
+  const [selectedStatus, setSelectedStatus] = useState('')
+
+  useEffect(() => {
+    if (selectedStatus){
+      dispatch(listProducts(undefined,undefined,undefined,undefined,selectedStatus))
+    }else{
+      dispatch(listProducts())
+    }
+  },[dispatch,selectedStatus])
+
   return (
     <>
+        <Row className='justify-content-end'>
+          <Col md={4}>
+            <Row>
+            {statuses.map((status) => (
+              <Col>
+                {status.id !== 3 ?                                        
+                  <Form.Check
+                      type='radio'
+                      id={status.id.toString()}
+                      value={status.value.toString()}
+                      label={status.name}
+                      name='statusGroup'
+                      onChange={(e) => setSelectedStatus(e.target.value)}
+                  />                                        
+                :                                       
+                  <Form.Check
+                      defaultChecked
+                      type='radio'
+                      id={status.id.toString()}
+                      value={status.value.toString()}
+                      label={status.name}
+                      name='statusGroup'
+                      onChange={(e) => setSelectedStatus(e.target.value)}
+                  />
+                }
+              </Col>
+            ))}
+            </Row>
+          </Col>
+        </Row>
         { loading ? <Loader />
         : error ? <Message variant='danger'>{error}</Message>  
         : <Row>
