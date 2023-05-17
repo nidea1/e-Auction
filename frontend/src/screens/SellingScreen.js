d685340206008f1b940313bacddb80a0d0c6dfca
nidea1 <crlidoruk@gmail.com>
2023-05-17 22:38:24 +0300

    user-friendly interface update

--- frontend/src/screens/SellingScreen.js
+++ frontend/src/screens/SellingScreen.js
@@ -44,46 +44,47 @@ function SellingScreen() {
     return (
         <>
             <Row className='mx-3'>
-                <Row className='border-bottom pb-2'>
-                    <Col md={4} className='fw-semibold'>
-                        You listing {products.length} {products.length === 1 ? 'product:': 'products:'}
-                    </Col>
-                    <Col md={8} className='d-flex flex-column justify-content-end'>
-                        <Row>
-                            {statuses.map((status) => (
-                                <Col>
-                                    {status.id !== 3 ?                                        
-                                        <Form.Check
-                                            type='radio'
-                                            id={status.id.toString()}
-                                            value={status.value.toString()}
-                                            label={status.name}
-                                            name='statusGroup'
-                                            onChange={(e) => setSelectedStatus(e.target.value)}
-                                        />                                        
-                                    :                                       
-                                        <Form.Check
-                                            defaultChecked
-                                            type='radio'
-                                            id={status.id.toString()}
-                                            value={status.value.toString()}
-                                            label={status.name}
-                                            name='statusGroup'
-                                            onChange={(e) => setSelectedStatus(e.target.value)}
-                                        />
-                                    }
-                                </Col>
-                            ))}
-                        </Row>
-                    </Col>
-                </Row>
-                <Row className='my-3'>
-                {products.map((product) => (
-                  <Col key={product._id} sm={12} md={6} lg={4}>
+                <Col md={4} className='fw-semibold'>
+                    You listing {products.length} {products.length === 1 ? 'product:': 'products:'}
+                </Col>
+                <Col md={8} className='d-flex flex-column justify-content-end'>
+                    <Row>
+                        {statuses.map((status) => (
+                            <Col>
+                                {status.id !== 3 ?                                        
+                                    <Form.Check
+                                        type='radio'
+                                        id={status.id.toString()}
+                                        value={status.value.toString()}
+                                        label={status.name}
+                                        name='statusGroup'
+                                        onChange={(e) => setSelectedStatus(e.target.value)}
+                                    />                                        
+                                :                                       
+                                    <Form.Check
+                                        defaultChecked
+                                        type='radio'
+                                        id={status.id.toString()}
+                                        value={status.value.toString()}
+                                        label={status.name}
+                                        name='statusGroup'
+                                        onChange={(e) => setSelectedStatus(e.target.value)}
+                                    />
+                                }
+                            </Col>
+                        ))}
+                    </Row>
+                </Col>
+            </Row>
+            <Col className='d-flex justify-content-center'>
+                <hr className='divider'/>
+            </Col>   
+            <Row className='mx-3'>
+            {products.map((product) => (
+                <Col key={product._id} sm={12} md={6} lg={4}>
                     <ProductByCategory product={product} />
-                  </Col>
-                ))}
-              </Row>
+                </Col>
+            ))}
             </Row>
         </>
     )
