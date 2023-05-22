754cde22d475d44be576a9995dae93091645a7ec
nidea1 <crlidoruk@gmail.com>
2023-05-22 12:31:19 +0300

    added filter fields

--- backend/base/views/bid_views.py
+++ backend/base/views/bid_views.py
@@ -3,10 +3,15 @@ from ..models import Bid
 
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 class BidList(ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = BidSerializer
+
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ['productName']
+    ordering_fields = ['createdAt']
     
     def get_queryset(self):
         return Bid.objects.filter(user=self.request.user)
