eb92de07f686599b41f0abe2371a5bcd24689509
nidea1 <crlidoruk@gmail.com>
2023-05-29 17:23:45 +0300

    added email settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -247,7 +247,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True  
 EMAIL_HOST = 'smtp.gmail.com'  
-EMAIL_HOST_USER = 'eauction.nidea1@gmail.com'  
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587  
 
