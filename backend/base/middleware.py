c49b6c26fce341ab6c2acc907234ba31b0bbf830
nidea1 <crlidoruk@gmail.com>
2023-06-19 16:20:37 +0300

    bug fixed

--- backend/base/middleware.py
+++ backend/base/middleware.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from django.utils.deprecation import MiddlewareMixin
-from oauth2_provider.models import RefreshToken
+from oauth2_provider.models import RefreshToken, AccessToken
 from dotenv import load_dotenv
 load_dotenv()
 import os
@@ -12,37 +13,40 @@ class CustomCoookieMiddleware(MiddlewareMixin):
         base_url = os.environ.get('BASE_API_URL')
 
         if token:
-            request.META['HTTP_AUTHORIZATION'] = f'Bearer {token}'
+            access_token = AccessToken.objects.filter(token=token, expires__gte = datetime.now())
+            if not access_token:
+                refresh_token_str = request.COOKIES.get('refresh_token')
+                refresh_token = RefreshToken.objects.get(token=refresh_token_str)
+                refresh_token.delete()
+                request._delete_token = True
+            else:
+                request.META['HTTP_AUTHORIZATION'] = f'Bearer {token}'
         else:
             refresh_token_str = request.COOKIES.get('refresh_token')
             if refresh_token_str:
-                try:
-                    data = {
-                        'grant_type': 'refresh_token',
-                        'client_id': os.environ.get('MAIN_AUTH_KEY'),
-                        'client_secret': os.environ.get('MAIN_AUTH_SECRET'),
-                        'refresh_token': refresh_token_str
-                    }
-                    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-                    response = requests.post(f'{base_url}/api/users/login/', data=data, headers=headers)
-                    
-                    if response.status_code == 200:
-                        access_token = response.json()['access_token']
-                        refresh_token = response.json()['refresh_token']
+                data = {
+                    'grant_type': 'refresh_token',
+                    'client_id': os.environ.get('MAIN_AUTH_KEY'),
+                    'client_secret': os.environ.get('MAIN_AUTH_SECRET'),
+                    'refresh_token': refresh_token_str
+                }
+                headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+                response = requests.post(f'{base_url}/api/users/login/', data=data, headers=headers)
+                
+                if response.status_code == 200:
+                    access_token = response.json()['access_token']
+                    refresh_token = response.json()['refresh_token']
 
-                        old_refresh_token = RefreshToken.objects.get(token=refresh_token_str)
-                        old_refresh_token.delete()
+                    old_refresh_token = RefreshToken.objects.get(token=refresh_token_str)
+                    old_refresh_token.delete()
 
-                        request.META['HTTP_AUTHORIZATION'] = f'Bearer {access_token}'
-                        request._new_access_token = access_token
-                        request._new_refresh_token = refresh_token
-                    else:
-                        refresh_token = RefreshToken.objects.get(token=refresh_token_str)
-                        refresh_token.delete()
-                        request._delete_token = True
-
-                except RefreshToken.DoesNotExist:
-                    pass
+                    request.META['HTTP_AUTHORIZATION'] = f'Bearer {access_token}'
+                    request._new_access_token = access_token
+                    request._new_refresh_token = refresh_token
+                else:
+                    refresh_token = RefreshToken.objects.get(token=refresh_token_str)
+                    refresh_token.delete()
+                    request._delete_token = True
 
     def process_response(self, request, response):
         if hasattr(request, '_new_access_token'):
