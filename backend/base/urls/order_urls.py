683ef66b4044af11fd0490b35e199b18a251c55f
nidea1 <crlidoruk@gmail.com>
2023-06-22 18:03:28 +0300

    new endpoint urls

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -6,5 +6,6 @@ from ..views.order_views import *
 urlpatterns = [
     path('<int:pk>', OrderDetail.as_view()),
     path('buying/', BuyingOrderList.as_view()),
-    path('confirmed/', ConfirmedOrderList.as_view()),
+    path('buyer/', ForBuyerOrderList.as_view()),
+    path('seller/', ForSellerOrderList.as_view()),
 ]
